101519e7492a71fe865f92a63c0de408849da806
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-04 19:25:17 +0100

    Created Booking Component and About to style it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HeroPage from './Components/HeroPage/HeroPage';
+import Booking from './Components/Booking/Booking';
 
 function App() {
   return (
     <div>
       <HeroPage/>
+      <Booking/>
     </div>
   );
 }
---
+++ src/Components/Booking/Booking.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Booking = () => {
+  return (
+    <div>Booking</div>
+  )
+}
+
+export default Booking
