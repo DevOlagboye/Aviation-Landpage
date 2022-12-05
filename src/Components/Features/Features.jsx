282e4507dad46a36467c43a8980d31ab8705fd6f
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-05 14:26:11 +0100

    Added some icons and still working on the Features card

--- src/Components/Features/Features.jsx
+++ src/Components/Features/Features.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Features.css"
+import bestGuideIcon from "../../Assets/images/Best Guide.png"
 
 const Booking = () => {
   return (
@@ -7,7 +8,9 @@ const Booking = () => {
         <h4 className='our-feature-main-text'>Our Features</h4>
         <div className='features-cards'>
           <div className='feature-card'>
-            
+            <img src={bestGuideIcon} alt="Best Guide Icon" />
+            <h5>Best Guide</h5>
+            <p>Suspendisse ultrices nibh <br/> non cursus sagittis. Morbi <br/>dictum consequat ex, quis <br/>finibus magna.</p>
           </div>
         </div>
     </div>
