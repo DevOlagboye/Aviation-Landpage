20e68de921b77bdf0cf5b94db1d04c489721accf
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-06 12:42:01 +0100

    Added the Call to Action Compoenent to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import HeroPage from './Components/HeroPage/HeroPage';
-import Features from "./Components/Features/Features"
+import Features from "./Components/Features/Features";
+import Cta from './Components/Cta/Cta';
 
 function App() {
   return (
     <div>
       <HeroPage/>
       <Features/>
+      <Cta/>
     </div>
   );
 }
