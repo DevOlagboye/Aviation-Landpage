061f1df63f25f3cd328b475f9278fc127cf7b5aa
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-06 13:56:37 +0100

    Almost done with the Call to Action component

--- src/Components/Cta/Cta.jsx
+++ src/Components/Cta/Cta.jsx
@@ -12,10 +12,23 @@ const Cta = () => {
         <div className='cta-details'>
             <h5>Let's Fly <img src={ctaLine} alt="CTA Line" /></h5> 
             <h4>It’s one of the leading online flight <br/> booking platforms in the world</h4>
+            <div className='cta-more-details'>
+                <div className='circle'></div>
             <p>Contrary to popular belief, Lorem Ipsum is not simply random text</p>
+            </div>
+            <div className='cta-more-details'>
+            <div className='circle'></div>
             <p>If you are going to use a passage of Lorem Ipsum</p>
+            </div>
+            <div className='cta-more-details'>
+            <div className='circle'></div>
             <p>Lorem Ipsum is therefore always free from repetition, injected<br/> humour, or non-characteristic words etc.</p>
-            <p>It is a long established fact that a reader will be distracted <br/>by the readable content of a page when looking at its layout.</p>
+            </div>
+            <div className='cta-more-details'>
+                <div className='circle'></div>
+                <p>It is a long established fact that a reader will be distracted <br/>by the readable content of a page when looking at its layout.</p>
+            </div>
+            <button>Book Now</button>
         </div>
     </div>
   )
