4f372fd7ece620e2e0f4ceff753281eba8da19bf
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-06 12:51:37 +0100

    Added some elements needed for the Call to Action Component

--- src/Components/Cta/Cta.jsx
+++ src/Components/Cta/Cta.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import "./Cta.css"
+import ctaImage from "../../Assets/images/Cta-image.png"
+import ctaLine from "../../Assets/images/cta-line.png"
 
 const Cta = () => {
   return (
-    <div>Cta</div>
+    <div className='cta-container'>
+        <div className='cta-image'>
+            <img src={ctaImage} alt="Call to Action" />
+        </div>
+        <div className='cta-details'>
+            <h5>Let's Fly <img src={ctaLine} alt="CTA Line" /></h5> 
+            <h5>It’s one of the leading online flight <br/> booking platforms in the world</h5>
+        </div>
+    </div>
   )
 }
 
