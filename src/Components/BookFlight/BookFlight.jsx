348954d1fcb6f05e53faf6279e9231904e36e352
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-07 15:51:28 +0100

    Started working on the sketch

---
+++ src/Components/BookFlight/BookFlight.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BookFlight = () => {
+  return (
+    <div className='book-flight-container'>
+        <div className='book-flight-details'>
+
+        </div>
+    </div>
+  )
+}
+
+export default BookFlight
