52083ca4f5da3196ed6cdebac2bc2bb3f55e777e
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-07 17:13:30 +0100

    Added fonts to the BookFlight component

--- src/Components/BookFlight/BookFlight.jsx
+++ src/Components/BookFlight/BookFlight.jsx
@@ -1,21 +1,9 @@
-import React, {useState} from 'react'
-import {Carousel, Radio} from "antd"
+import React from 'react'
 import AirDeliveryImage from "../../Assets/images/Air Delivery.png"
 import TicketImage from "../../Assets/images/ticket-background.png"
 import "./BookFlight.css"
 
-const contentStyle = {
-    height: '160px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
-  };
 const BookFlight = () => {
-    const [dotPosition, setDotPosition] = useState('right');
-    const handlePositionChange = ({ target: { value } }) => {
-        setDotPosition(value);
-      };
   return (
     <div className='book-flight-container'>
         <div className='book-flight-details'>
@@ -25,20 +13,6 @@ const BookFlight = () => {
                 <img src={AirDeliveryImage} alt="Air Craft Delivery" />
             </div>
             <div className='book-flight-ticket'>
-            <Carousel dotPosition={dotPosition} autoplay>
-        <div>
-          <h3 style={contentStyle}>1</h3>
-        </div>
-        <div>
-          <h3 style={contentStyle}>2</h3>
-        </div>
-        <div>
-          <h3 style={contentStyle}>3</h3>
-        </div>
-        <div>
-          <h3 style={contentStyle}>4</h3>
-        </div>
-      </Carousel>
                 <img src={TicketImage} alt="Book Flight Ticket" />
             </div>
         </div>
