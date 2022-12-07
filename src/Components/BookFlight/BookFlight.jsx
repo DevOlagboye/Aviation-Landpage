d1ba5a7904cce2dca0d41ec5bf4f1cf4b60a0727
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-07 16:00:42 +0100

    Added more information to the BookFlight Component

--- src/Components/BookFlight/BookFlight.jsx
+++ src/Components/BookFlight/BookFlight.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import AirDeliveryImage from "../../Assets/images/Air Delivery.png"
+import "./BookFlight.css"
 
 const BookFlight = () => {
   return (
     <div className='book-flight-container'>
         <div className='book-flight-details'>
-
+            <div className='book-flight-text-image'>
+                <h4>Book Popular Flight <br/> Tickets</h4>
+                <p>Lorem Ipsum is simply dummy text of the printing and <br/>typesetting industry. Lorem Ipsum has been the industry's</p>
+                <img src={AirDeliveryImage} alt="Air Craft Delivery" />
+            </div>
         </div>
     </div>
   )
