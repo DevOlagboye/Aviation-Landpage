090e5880e9f812b41447fa14cf297b21b4893ff6
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-07 16:15:27 +0100

    Working more on the layout of the book flight component

--- src/Components/BookFlight/BookFlight.jsx
+++ src/Components/BookFlight/BookFlight.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import AirDeliveryImage from "../../Assets/images/Air Delivery.png"
+import TicketImage from "../../Assets/images/ticket-background.png"
 import "./BookFlight.css"
 
 const BookFlight = () => {
@@ -10,6 +11,9 @@ const BookFlight = () => {
                 <h4>Book Popular Flight <br/> Tickets</h4>
                 <p>Lorem Ipsum is simply dummy text of the printing and <br/>typesetting industry. Lorem Ipsum has been the industry's</p>
                 <img src={AirDeliveryImage} alt="Air Craft Delivery" />
+            </div>
+            <div className='book-flight-ticket'>
+                <img src={TicketImage} alt="Book Flight Ticket" />
             </div>
         </div>
     </div>
