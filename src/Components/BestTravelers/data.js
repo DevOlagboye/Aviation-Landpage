cc8e617b8bafe72dd1405ad2dd85d73fa90ae857
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-08 15:01:07 +0100

    All Best Travelers are now fetched from the data.js

--- src/Components/BestTravelers/data.js
+++ src/Components/BestTravelers/data.js
@@ -1,7 +1,11 @@
 import BestTraveler1Profile from "../../Assets/images/BestTraveler1-profile.png"
+import BestTraveler2Profile from "../../Assets/images/BestTraveler2-profile.png"
+import BestTraveler3Profile from "../../Assets/images/BestTraveler3-profile.png"
+import BestTraveler4Profile from "../../Assets/images/BestTraveler4-profile.png"
 import BestTravelerLocation1 from "../../Assets/images/BestTraveler-location1.png"
 import BestTravelerLocation2 from "../../Assets/images/BestTraveler-location2.png"
-import BestTraveler2Profile from "../../Assets/images/BestTraveler2-profile.png"
+import BestTravelerLocation3 from "../../Assets/images/BestTraveler-location3.png"
+import BestTravelerLocation4 from "../../Assets/images/BestTraveler-location4.png"
 export const bestTravelers = [{
         name: "Raju Mullah",
         username: "@rajumullah",
@@ -14,6 +18,18 @@ export const bestTravelers = [{
         username: "@zairevetrovs",
         profileImage: BestTraveler2Profile,
         location: BestTravelerLocation2
+    },
+    {
+        name: "Zaire Vetrovs",
+        username: "@zairevetrovs",
+        profileImage: BestTraveler3Profile,
+        location: BestTravelerLocation3
+    },
+    {
+        name: "Davis Schleifer",
+        username: "@davisschleifer",
+        profileImage: BestTraveler4Profile,
+        location: BestTravelerLocation4
     }
 
 ]
