1513b54febcb9248355ff1a1836721144f09c7a9
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-08 13:51:54 +0100

    Started adding contents to the Best Travelers Component

--- src/Components/BestTravelers/BestTravelers.jsx
+++ src/Components/BestTravelers/BestTravelers.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const BestTravelers = () => {
   return (
-    <div>BestTravelers</div>
+    <div className='best-traveler-container'>
+        <h4>Best Travelers Of This Month</h4>
+    </div>
   )
 }
 
