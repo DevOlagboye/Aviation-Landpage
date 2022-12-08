43e5b772b31c0ba747d268fab64a1af8c2dc16f2
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-08 14:44:34 +0100

    Successfully made the best travelers component loop its data from the data.js so once the data.js is upated it the best traveler component too will be updated

--- src/Components/BestTravelers/BestTravelers.jsx
+++ src/Components/BestTravelers/BestTravelers.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import "./BestTravelers.css"
-import BestTravelerLocation1 from "../../Assets/images/BestTraveler-location1.png"
-import BestTraveler1Profile from "../../Assets/images/BestTraveler1-profile.png"
+import { besTravelers } from './data'
+
+
 
 const BestTravelers = () => {
   return (
     <div className='best-traveler-container'>
         <h4>Best Travelers Of This Month</h4>
     <div className='best-traveler-gallery'>
-        <div className='best-traveler-card'>
-            <img src={BestTravelerLocation1} alt="BestTraveler Location" className='best-traveler-location'/>
-            <img src={BestTraveler1Profile} alt="BestTraveler Profile" className='best-traveler-profile' />
-            <h5>Raju Mullah</h5>
-            <p>@rajumulllah</p>
+        {besTravelers.map(bestTraveler => (
+            <div className='best-traveler-card'>
+            <img src={bestTraveler.location} alt="BestTraveler Location" className='best-traveler-location'/>
+            <img src={bestTraveler.profileImage} alt="BestTraveler Profile" className='best-traveler-profile' />
+            <h5>{bestTraveler.name}</h5>
+            <p>{bestTraveler.username}</p>
         </div>
+        ))}
 
     </div>
     </div>
---
+++ src/Components/BestTravelers/data.js
@@ -0,0 +1,17 @@
+import BestTraveler1Profile from "../../Assets/images/BestTraveler1-profile.png"
+import BestTravelerLocation1 from "../../Assets/images/BestTraveler-location1.png"
+export const besTravelers = [{
+        name: "Raju Mullah",
+        username: "@rajumullah",
+        profileImage: BestTraveler1Profile,
+        location: BestTravelerLocation1
+    },
+
+    {
+        name: "Raju Mullah",
+        username: "@rajumullah",
+        profileImage: BestTraveler1Profile,
+        location: BestTravelerLocation1
+    }
+
+]
