184e457fcea39d4ff24037b487c061c11fdd6133
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-08 14:04:12 +0100

    Started styling the Best Traveler component

--- src/Components/BestTravelers/BestTravelers.jsx
+++ src/Components/BestTravelers/BestTravelers.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import "./BestTravelers.css"
 
 const BestTravelers = () => {
   return (
     <div className='best-traveler-container'>
         <h4>Best Travelers Of This Month</h4>
+    <div className='best-traveler-gallery'>
+        <div className='best-traveler-card'>
+            
+
+        </div>
+
+    </div>
     </div>
   )
 }
