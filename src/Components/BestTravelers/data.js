d2a543f43cf6508f07bf887d2040979154d5bf62
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-08 14:50:51 +0100

    Started adding more data to the data.js where the Best Travelers is coming from

--- src/Components/BestTravelers/data.js
+++ src/Components/BestTravelers/data.js
@@ -1,6 +1,7 @@
 import BestTraveler1Profile from "../../Assets/images/BestTraveler1-profile.png"
 import BestTravelerLocation1 from "../../Assets/images/BestTraveler-location1.png"
 import BestTravelerLocation2 from "../../Assets/images/BestTraveler-location2.png"
+import BestTraveler2Profile from "../../Assets/images/BestTraveler2-profile.png"
 export const bestTravelers = [{
         name: "Raju Mullah",
         username: "@rajumullah",
@@ -9,10 +10,10 @@ export const bestTravelers = [{
     },
 
     {
-        name: "Raju Mullah",
-        username: "@rajumullah",
-        profileImage: BestTraveler1Profile,
-        location: BestTravelerLocation1
+        name: "Zaire Vetrovs",
+        username: "@zairevetrovs",
+        profileImage: BestTraveler2Profile,
+        location: BestTravelerLocation2
     }
 
 ]
