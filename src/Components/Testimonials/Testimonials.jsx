b5ed268a6e9bbf0ee7d6128f38c739a444ea83b1
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-09 16:44:25 +0100

    Added details required in the testimonials now and the carousel

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import CustomersImage from "../../Assets/images/Customers.png"
-import "./Testimonials.css"
+import CustomerProfile from "../../Assets/images/Customer-profile.png"
 import {Swiper, SwiperSlide} from "swiper/react"
+import {Rate} from "antd"
 import "swiper/css"
 import "swiper/css/pagination"
 import "swiper/css/navigation"
+import "./Testimonials.css"
 
 import {Autoplay, Pagination, Navigation} from "swiper"
 
@@ -28,7 +30,19 @@ const Testimonials = () => {
             modules= {[Autoplay, Pagination, Navigation]}
             className="mySwiper">
                 <SwiperSlide>
-        
+                    <div className='customer-details'>
+                    <div className='customer-profile'>
+                        <img src={CustomerProfile} alt="" />
+                    </div>
+                    <div className='customer-name-rating'>
+                        <h5>Jaylon Vaccaro</h5>
+                        <Rate defaultValue={5}/>
+                    </div>
+                    </div>
+                    <div className='testimonial-details'>
+                        <h5>There are many variations of passages of Lorem <br/>Ipsum available, but the majority have suffered <br/>alteration in some form, by injected humour, or <br/>randomised words which don</h5>
+                    </div>
+                    
                 </SwiperSlide>
                 <SwiperSlide>Slide 2</SwiperSlide>
                 <SwiperSlide>Slide 3</SwiperSlide>
