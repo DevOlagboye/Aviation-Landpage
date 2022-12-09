27b8ecaba55f601d33a32e448bc6a35ef40c0646
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-09 14:07:48 +0100

    Added the customers Image to the testimonials component

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import CustomersImage from "../../Assets/images/Customers.png"
 
 const Testimonials = () => {
   return (
     <div className='testimonial-container'>
         <h5>What's our customer saying</h5>
         <h3>Our Customer Feedback</h3>
+        <div className='testimonial-customers'>
+            <img src={CustomersImage} alt="Customer's Images" />
+        </div>
     </div>
   )
 }
