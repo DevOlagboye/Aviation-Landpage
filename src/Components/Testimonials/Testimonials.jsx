0c3201080f9771c4fafd849267b87b6fa017c2df
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-09 13:46:27 +0100

    Started adding content to the Testimonials components

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <div className='testimonial-container'>
+        <h5>What's our customer saying</h5>
+        <h3>Our Customer Feedback</h3>
+    </div>
   )
 }
 
