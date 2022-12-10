421f095b1a49b374a9b998df90d9aac4eb87fc2c
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-10 12:47:55 +0100

    Added all images needed for the Memories Component

--- src/Components/Memories/Memories.jsx
+++ src/Components/Memories/Memories.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Memories1 from "../../Assets/images/Memories1.png"
+import Memories2 from "../../Assets/images/Memories2.png"
+import Memories3 from "../../Assets/images/Memories3.png"
+import Memories4 from "../../Assets/images/Memories4.png"
 import "./Memories.css"
 
 const Memories = () => {
   return (
     <div className='memories-container'>
         <h4>Make Memories With Us</h4>
+        <div className='memories-cards'>
+            <div className='memory-card'>
+                <img src={Memories1} alt="" />
+            </div>
+        </div>
     </div>
   )
 }
