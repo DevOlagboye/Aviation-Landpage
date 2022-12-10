c6797c9da35bdeee9cfeaf48cc598094cd8876ae
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-10 12:36:51 +0100

    Imported the Memories.css

---
+++ src/Components/Memories/Memories.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Memories.css"
+
+const Memories = () => {
+  return (
+    <div className='memories-container'>
+
+    </div>
+  )
+}
+
+export default Memories
