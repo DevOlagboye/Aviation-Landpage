21cc60154d12ca288a32dc3cfc18567f5d4ca50a
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-11 14:01:32 +0100

    Setting up the Newsletter component and importing the CSS

---
+++ src/Components/Newsletter/Newsletter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Newsletter.css"
+const Newsletter = () => {
+  return (
+    <div className='newsletter-container'>
+        
+    </div>
+  )
+}
+
+export default Newsletter
