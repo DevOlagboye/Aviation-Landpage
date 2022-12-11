9418d76325bdbcc1de1d4a37893f2d9fbe074dfd
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-11 14:14:05 +0100

    Added all required JSX elements to the Newsletter Component

--- src/Components/Newsletter/Newsletter.jsx
+++ src/Components/Newsletter/Newsletter.jsx
@@ -4,6 +4,10 @@ const Newsletter = () => {
   return (
     <div className='newsletter-container'>
         <h4>Subscribe Newsletter & <br/> Get Latest News</h4>
+        <form>
+            <input type="email" name="" id="" />
+            <button>Subscribe</button>
+        </form>
     </div>
   )
 }
