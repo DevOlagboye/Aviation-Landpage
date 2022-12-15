3b0ccd04774e4f380dc0b193d6c9b5da2961f767
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-15 14:55:59 +0100

    Removed some props no more needed in the App.js

--- src/App.js
+++ src/App.js
@@ -9,20 +9,15 @@ import Testimonials from './Components/Testimonials/Testimonials';
 import Memories from './Components/Memories/Memories';
 import Newsletter from './Components/Newsletter/Newsletter';
 import Footer from './Components/Footer/Footer';
-import {gsap, Power3} from "gsap"
+import gsap from "gsap"
 
 function App() {
-  //Gsap
-  let App = useRef(null)
-  let tl = new gsap.timeline();
-  let ease = Power3.easeInOut;
-  //Gsap
   useEffect(() => {
     gsap.to(App, {visibility: "visible"})
   })
   return (
     <div className='App' ref={el => {App = el}}>
-      <HeroPage timeline={tl} ease={ease}/>
+      <HeroPage/>
       <Features/>
       <Cta/>
       <BookFlight/>
