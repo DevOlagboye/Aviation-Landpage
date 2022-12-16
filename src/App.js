468eb49e4801d46d4a86f05054f4cf7b39bb2fe9
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-16 13:17:15 +0100

    Refactoring the code to be able to use Scrolltrigger by Gsap

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
   })
   return (
     <div className='App' ref={el => {App = el}}>
-      <HeroPage el={sectionRef}/>
+      <HeroPage/>
       <Features/>
       <Cta/>
       <BookFlight/>
