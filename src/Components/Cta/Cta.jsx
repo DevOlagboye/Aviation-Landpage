07ca501396a9e8851cdfc5bb65840f7321619d66
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-16 16:48:24 +0100

    Fixed the Scrolltrigger Animation on Call to Action Component

--- src/Components/Cta/Cta.jsx
+++ src/Components/Cta/Cta.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, {useRef, useEffect}from 'react'
 import "./Cta.css"
 import ctaImage from "../../Assets/images/Cta-image.png"
 import ctaLine from "../../Assets/images/cta-line.png"
+import  {gsap, Power3} from "gsap"
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 const Cta = () => {
+    let ctaContainers = useRef(null)
+    gsap.registerPlugin(ScrollTrigger);
+    useEffect(() => {
+        gsap.fromTo(ctaContainers,{
+            opacity: 0,
+            scale: 0.4,
+            y: -20
+        },{
+            opacity: 1,
+            y: 0,
+            scale: 1,
+            duration: 5,
+            ease: Power3.easeIn,
+            scrollTrigger: {
+                markers: false,
+                trigger: ctaContainers,
+                 start: "-190%",
+                 end: "-100%",
+                scrub: true
+            }
+        })
+    }, [])
   return (
-    <div className='cta-container'>
+    <div className='cta-container' ref={el => {ctaContainers = el}}>
         <div className='cta-image'>
-            <img src={ctaImage} alt="Call to Action" />
+            <img src={ctaImage} alt="Call to Action"/>
         </div>
         <div className='cta-details'>
             <h5>Let's Fly <img src={ctaLine} alt="CTA Line" className='cta-line' /></h5> 
