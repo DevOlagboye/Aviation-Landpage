22386d53bb0b356244f2be011c91b771190d7036
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-17 17:01:18 +0100

    Removed some unused functions from App.js

--- src/App.js
+++ src/App.js
@@ -9,30 +9,10 @@ import Testimonials from './Components/Testimonials/Testimonials';
 import Memories from './Components/Memories/Memories';
 import Newsletter from './Components/Newsletter/Newsletter';
 import Footer from './Components/Footer/Footer';
-import {gsap, Power3} from "gsap"
+import {gsap} from "gsap"
 
 
 function App() {
-
-  const fadeIn = (element) => {
-    gsap.to(element, {duration: 1,
-      opacity: 1,
-      x: 0,
-      ease: Power3.easeOut,
-      stagger: {
-        amount: .3
-      }
-    
-    })
-  }
-  const fadeOut = (element) => {
-    gsap.to(element, {duration: 1,
-      opacity: 0,
-      x: -20,
-      ease: Power3.easeOut,
-      })
-  }
- // We've reached so animate
   useEffect(() => {
     gsap.to(App, {visibility: "visible"})
   })
