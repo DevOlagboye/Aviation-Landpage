3c60c55923b24cd8780aa097ae8e34167bcf6f62
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-19 11:08:43 +0100

    Fixed Circle sizing problem on the mobile

--- src/Components/Cta/Cta.jsx
+++ src/Components/Cta/Cta.jsx
@@ -37,20 +37,16 @@ const Cta = () => {
             <h5>Let's Fly <img src={ctaLine} alt="CTA Line" className='cta-line' /></h5> 
             <h4>It’s one of the leading online flight <br/> booking platforms in the world</h4>
             <div className='cta-more-details'>
-                <div className='circle'></div>
-            <p>Contrary to popular belief, Lorem Ipsum is not simply random text</p>
+            <p> <span className='circle'></span> Contrary to popular belief, Lorem Ipsum is not simply random text</p>
             </div>
             <div className='cta-more-details'>
-            <div className='circle'></div>
-            <p>If you are going to use a passage of Lorem Ipsum</p>
+            <p><span className='circle'></span> If you are going to use a passage of Lorem Ipsum</p>
             </div>
             <div className='cta-more-details'>
-            <div className='circle'></div>
-            <p>Lorem Ipsum is therefore always free from repetition, injected<br/> humour, or non-characteristic words etc.</p>
+            <p> <span className='circle'></span> Lorem Ipsum is therefore always free from repetition, injected<br/> humour, or non-characteristic words etc.</p>
             </div>
             <div className='cta-more-details'>
-                <div className='circle'></div>
-                <p>It is a long established fact that a reader will be distracted <br/>by the readable content of a page when looking at its layout.</p>
+                <p><span className='circle'></span> It is a long established fact that a reader will be distracted <br/>by the readable content of a page when looking at its layout.</p>
             </div>
             <button>Book Now</button>
         </div>
