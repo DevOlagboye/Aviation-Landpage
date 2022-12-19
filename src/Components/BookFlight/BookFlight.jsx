df1972d6340022a73e16baf4d226af35c9fbded9
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-19 11:13:52 +0100

    Added Animation to BookFlight component

--- src/Components/BookFlight/BookFlight.jsx
+++ src/Components/BookFlight/BookFlight.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 import AirDeliveryImage from "../../Assets/images/Air Delivery.png"
 import TicketImage from "../../Assets/images/ticket-background.png"
 import "./BookFlight.css"
+import  {gsap, Power3} from "gsap"
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 const BookFlight = () => {
+  let bookFlight = useRef(null)
+    gsap.registerPlugin(ScrollTrigger);
+    useEffect(() => {
+        gsap.fromTo(bookFlight,{
+            opacity: 0,
+            scale: 0.5,
+            y: -20
+        },{
+            opacity: 1,
+            y: 0,
+            scale: 1,
+            duration: 5,
+            ease: Power3.easeIn,
+            scrollTrigger: {
+                markers: false,
+                trigger: bookFlight,
+                 start: "-190%",
+                 end: "-100%",
+                scrub: true
+            }
+        })
+    }, [])
   return (
-    <div className='book-flight-container'>
+    <div className='book-flight-container' ref={el => {bookFlight = el}}>
         <div className='book-flight-details'>
             <div className='book-flight-text-image'>
                 <h4>Book Popular Flight <br/> Tickets</h4>
