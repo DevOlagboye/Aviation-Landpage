db28b6e072459611b7271e404ac9646a11337a76
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-21 14:06:45 +0100

    Added the Success message as well the navigation back home

--- src/Components/SuccessPage/SuccessPage.jsx
+++ src/Components/SuccessPage/SuccessPage.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import {Alert} from 'antd'
+import { useNavigate } from 'react-router-dom'
 import "./SucessPage.css"
 
 const SuccessPage = () => {
+   const navigate = useNavigate();
+    const handleClose = () =>{
+        navigate("/")
+    }
   return (
-    <div>
-         <Alert
+    <div className='success-container'>
+         <Alert className='success-alert' onClose={handleClose}
       message="Book Successful"
       description="Hi there, You've Successful booked your ticket kindly check your gmail for mor information from us"
       type="success"
       showIcon
+      closable
     />
     </div>
   )
