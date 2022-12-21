31c887d4c73bd6382391685c4dd6e93a673e5c99
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-21 12:38:39 +0100

    Installed React-router-dom for routing

--- src/Components/HeroPage/HeroPage.jsx
+++ src/Components/HeroPage/HeroPage.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useMemo, useRef, useEffect} from 'react'
+import {useNavigate} from "react-router-dom"
 import Select from "react-select"
 import countryList from "react-select-country-list"
 import { DatePicker, Drawer } from 'antd'
