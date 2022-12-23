a695e1f29d8b83ab89d021a745faab7d296829ac
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-23 20:38:54 +0100

    Added memories to be fetched from data.js

--- src/Components/Memories/data.js
+++ src/Components/Memories/data.js
@@ -2,7 +2,7 @@ import memories1 from "../../Assets/images/Memories1.png"
 import memories2 from "../../Assets/images/Memories2.png"
 import memories3 from "../../Assets/images/Memories3.png"
 import memories4 from "../../Assets/images/Memories4.png"
-export const Memories = [{
+export const memoriesImages = [{
         memoryImage: memories1,
     },
     {
