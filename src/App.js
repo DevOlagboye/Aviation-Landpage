d4ec5437b60f99643c739ba6538bcead4da597ab
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-24 17:32:29 +0100

    Adding more functionality to the aviation site

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import React from 'react';
-import HeroPage from './Components/HeroPage/HeroPage';
-import SuccessPage from './Components/SuccessPage/SuccessPage';
-
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import HeroPage from "./Components/HeroPage/HeroPage";
+import SuccessPage from "./Components/SuccessPage/SuccessPage";
 
 function App() {
-  
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<HeroPage/>}/>
-          <Route path="success" element={<SuccessPage/>}/>
+          <Route path="/" element={<HeroPage />} />
+          <Route path="/success" element={<SuccessPage />} />
+          <Route path="/passengers" element={<SuccessPage />} />
         </Routes>
       </Router>
     </div>
