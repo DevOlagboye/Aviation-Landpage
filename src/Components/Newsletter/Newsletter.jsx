2764e7f1778dd08ad4835afa7cf45575f92610e2
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-25 18:48:04 +0100

    Added Success Message to Newsletter

--- src/Components/Newsletter/Newsletter.jsx
+++ src/Components/Newsletter/Newsletter.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import "./Newsletter.css"
+import {message} from "antd"
 const Newsletter = () => {
+  const [messageApi, contextHolder] = message.useMessage();
+  const handleNewsletter = (e) =>{
+    e.preventDefault()
+    messageApi
+        .open({
+          type: "loading",
+          content: "Confirming",
+          duration: 2.5,
+        })
+        .then(() => message.success("Thanks for subscribing...", 2.5))
+  }
   return (
     <div className='newsletter-container'>
         <h4>Subscribe Newsletter & <br/> Get Latest News</h4>
-        <form>
+        <form action='#'>
             <input type="email" name="" id="" placeholder="Enter your email address"/>
-            <button>Subscribe</button>
+            {contextHolder}
+            <button onClick={handleNewsletter}>Subscribe</button>
         </form>
     </div>
   )
