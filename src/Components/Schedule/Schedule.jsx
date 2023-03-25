c9e79e9d5f7bbfad02d6309b1007037488b65644
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-25 13:45:26 +0100

    Started working on the schedule Component

--- src/Components/Schedule/Schedule.jsx
+++ src/Components/Schedule/Schedule.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Schedule.css";
 import leftGradient from "../../Assets/images/gradient-left.png"
+import rightGradient from "../../Assets/images/gradient-right.png"
 
 const Schedule = () => {
   return (
     <div className="schedule-container">
       <div className="gradient-container">
         <img src={leftGradient} alt="" className="left-gradient"/>
+        <img src={rightGradient} alt="" className="right-gradient"/>
       </div>
       <div className="schedule-form"></div>
     </div>
