82b6049eb21d65b2ebe402ab42141ede4e8d6ef9
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-25 18:57:10 +0100

    Added Success Message to Newsletter

--- src/Components/Newsletter/Newsletter.jsx
+++ src/Components/Newsletter/Newsletter.jsx
@@ -1,28 +1,43 @@
-import React from 'react'
-import "./Newsletter.css"
-import {message} from "antd"
+import React, { useRef } from "react";
+import "./Newsletter.css";
+import { message } from "antd";
 const Newsletter = () => {
   const [messageApi, contextHolder] = message.useMessage();
-  const handleNewsletter = (e) =>{
-    e.preventDefault()
-    messageApi
+  let newsLetterInput = useRef();
+  const handleNewsletter = (e) => {
+    e.preventDefault();
+    if (newsLetterInput.value === "") {
+      newsLetterInput.style = "border: 1px solid red";
+    } else {
+      messageApi
         .open({
           type: "loading",
           content: "Confirming",
           duration: 2.5,
         })
-        .then(() => message.success("Thanks for subscribing...", 2.5))
-  }
+        .then(() => message.success("Thanks for subscribing...", 2.5));
+    }
+  };
   return (
-    <div className='newsletter-container'>
-        <h4>Subscribe Newsletter & <br/> Get Latest News</h4>
-        <form action='#'>
-            <input type="email" name="" id="" placeholder="Enter your email address"/>
-            {contextHolder}
-            <button onClick={handleNewsletter}>Subscribe</button>
-        </form>
+    <div className="newsletter-container">
+      <h4>
+        Subscribe Newsletter & <br /> Get Latest News
+      </h4>
+      <form action="#">
+        <input
+          ref={(el) => {
+            newsLetterInput = el;
+          }}
+          type="email"
+          name=""
+          id=""
+          placeholder="Enter your email address"
+        />
+        {contextHolder}
+        <button onClick={handleNewsletter}>Subscribe</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Newsletter
+export default Newsletter;
