ae91ba0201a3a92e3c7ccb52d8e8e0701693fe0e
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-25 13:37:34 +0100

    Started working on the schedule Component

--- src/Components/Schedule/Schedule.jsx
+++ src/Components/Schedule/Schedule.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import "./Schedule.css"
+import React from "react";
+import "./Schedule.css";
+import leftGradient from "../../Assets/images/gradient-left.png"
 
 const Schedule = () => {
   return (
-    <div>Schedule</div>
-  )
-}
+    <div className="schedule-container">
+      <div className="gradient-container">
+        <img src={leftGradient} alt="" className="left-gradient"/>
+      </div>
+      <div className="schedule-form"></div>
+    </div>
+  );
+};
 
-export default Schedule
+export default Schedule;
