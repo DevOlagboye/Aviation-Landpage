62e2b3fe432826d17b19a72ba1cbb51f60dfe94e
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-25 14:24:09 +0100

    Started working on the schedule Component

--- src/Components/Schedule/Schedule.jsx
+++ src/Components/Schedule/Schedule.jsx
@@ -21,7 +21,9 @@ const Schedule = () => {
           </h4>
         </div>
       </div>
-      <div className="schedule-form"></div>
+      <div className="schedule-form">
+        <h5>Schedule your Flight</h5>
+      </div>
     </div>
   );
 };
