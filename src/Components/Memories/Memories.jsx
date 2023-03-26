f1397c9a69513b87ed36648095d2d913a588c804
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-26 16:27:41 +0100

    Finished the logic to request for traveling from and to before submitting

--- src/Components/Memories/Memories.jsx
+++ src/Components/Memories/Memories.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import "./Memories.css"
-import { memoriesImages } from './data'
+import React from "react";
+import "./Memories.css";
+import { memoriesImages } from "./data";
 
 const Memories = () => {
   return (
-    <div className='memories-container'>
-        <h4>Make Memories With Us</h4>
-        <div className='memories-cards'>
-            {memoriesImages.map(Memory => (
-                <div className='memories-card'>
-                <img src={Memory.memoryImage} alt="" />
-            </div>
-            ))}
-        </div>
+    <div className="memories-container">
+      <h4>Make Memories With Us</h4>
+      <div className="memories-cards">
+        {memoriesImages.map((Memory) => (
+          <div className="memories-card" key={Memory.id}>
+            <img src={Memory.memoryImage} alt="" />
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Memories
+export default Memories;
--- src/Components/Memories/data.js
+++ src/Components/Memories/data.js
@@ -3,15 +3,19 @@ import memories2 from "../../Assets/images/Memories2.png"
 import memories3 from "../../Assets/images/Memories3.png"
 import memories4 from "../../Assets/images/Memories4.png"
 export const memoriesImages = [{
+        id: 1,
         memoryImage: memories1,
     },
-    {
+    {   
+        id: 2,
         memoryImage: memories2,
     },
     {
+        id: 3,
         memoryImage: memories3,
     },
     {
+        id: 4,
         memoryImage: memories4
     }
 
