ac51c0dbf83ef4581e5538a3992fe84090c0dc78
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-30 15:42:25 +0100

    Removed some lagging animations

--- src/Components/BestTravelers/BestTravelers.jsx
+++ src/Components/BestTravelers/BestTravelers.jsx
@@ -1,52 +1,36 @@
-import React, {useRef, useEffect} from 'react'
-import "./BestTravelers.css"
-import {gsap, Power3} from "gsap"
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { bestTravelers } from './data'
-
-
+import React, { useRef, useEffect } from "react";
+import "./BestTravelers.css";
+import { gsap, Power3 } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { bestTravelers } from "./data";
 
 const BestTravelers = () => {
   gsap.registerPlugin(ScrollTrigger);
 
-  let bestTravelerContainer = useRef(null)
-
-  useEffect(() =>{
-    gsap.fromTo(bestTravelerContainer,
-      {
-        duration: 5,
-        opacity: 0
-      },{
-        y: -20,
-        opacity: 1,
-        ease: Power3.easeIn,
-        scrollTrigger: {
-          trigger: bestTravelerContainer,
-          markers: false,
-          start: "0%",
-          end: "7%",
-          scrub: true,
-          delay: 3
-        }
-    
-    });
-  }, [])
+  let bestTravelerContainer = useRef(null);
   return (
-    <div className='best-traveler-container' ref={el => {bestTravelerContainer = el}}>
-        <h4>Best Travelers Of This Month</h4>
-    <div className='best-traveler-gallery'>
-        {bestTravelers.map(bestTraveler => (
-            <div className='best-traveler-card' key={bestTraveler.name}>
-            <img src={bestTraveler.location} alt="BestTraveler Location" className='best-traveler-location'/>
-            <img src={bestTraveler.profileImage} alt="BestTraveler Profile" className='best-traveler-profile' />
+    <div className="best-traveler-container">
+      <h4>Best Travelers Of This Month</h4>
+      <div className="best-traveler-gallery">
+        {bestTravelers.map((bestTraveler) => (
+          <div className="best-traveler-card" key={bestTraveler.name}>
+            <img
+              src={bestTraveler.location}
+              alt="BestTraveler Location"
+              className="best-traveler-location"
+            />
+            <img
+              src={bestTraveler.profileImage}
+              alt="BestTraveler Profile"
+              className="best-traveler-profile"
+            />
             <h5>{bestTraveler.name}</h5>
             <p>{bestTraveler.username}</p>
-        </div>
+          </div>
         ))}
-
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default BestTravelers
+export default BestTravelers;
